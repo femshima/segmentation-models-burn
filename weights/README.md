58e4d4fea481a4a66a90ef133ca6892f3e245170
femshima <49227365+femshima@users.noreply.github.com>
2024-11-20 08:26:27 +0900

    add docs for weights

--- weights/README.md
+++ weights/README.md
@@ -1,3 +1,10 @@
 # Weight converter
 
 Convert the older pytorch models to the newer one, supported by candle.
+
+## Usage
+
+1. Install dependency (only `torch` is needed).
+1. Move to `input` and Run `download-efficientnet.sh` (without advprop) or `download-efficientnetadv.sh` (with advprop).
+1. Return to `weights` and run `python -m weights`.
+1. Copy the files in the `output` folder to `~/.cache/efficientnet-burn/`.
---
+++ weights/input/download-efficientnetadv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/adv-efficientnet-b0-b64d5a18.pth
+wget https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/adv-efficientnet-b1-0f3ce85a.pth
+wget https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/adv-efficientnet-b2-6e9d97e5.pth
+wget https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/adv-efficientnet-b3-cdd7c0f4.pth
+wget https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/adv-efficientnet-b4-44fb3a87.pth
+wget https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/adv-efficientnet-b5-86493f6b.pth
+wget https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/adv-efficientnet-b6-ac80338e.pth
+wget https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/adv-efficientnet-b7-4652b6dd.pth
+wget https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/adv-efficientnet-b8-22a8fe65.pth
